9db17bd452f43081cf6cad09a83d0d4f9ff531fd
kieran <kieran6roberts@gmail.com>
2024-03-27 20:19:48 +0000

    refactor: add gql api codegen

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -1,95 +1,20 @@
-const BLOG_HOSTNAME = "blog.kieranroberts.dev"
-const HASHNODE_API_URL = "https://gql.hashnode.com"
+import { BlogPostsDocument, PostPagePostsDocument, type PublicationPostConnection, type PageInfo, type PostEdge, type Post } from "generated/graphql";
+import { request } from 'graphql-request'
+
+const BLOG_HOSTNAME = "blog.kieranroberts.dev";
+const HASHNODE_API_URL = "https://gql.hashnode.com";
 
 export const INITIAL_POST_COUNT = 10;
 
-const postPagePostsQuery = `
- query PostPagePosts($host: String!, $first: Int!, $after: String) {
-  publication(host: $host) {
-    id
-    posts(first: $first, after: $after) {
-      edges {
-        node {
-          slug
-          content {
-           markdown
-         }
-         title
-         subtitle
-         coverImage {
-          url
-         }
-         readTimeInMinutes
-        }
-        cursor
-      }
-      pageInfo {
-       hasNextPage
-       endCursor
-     }
-    }
-  }
- }`
-;
-
-const postsQuery = `
- query BlogPosts($host: String!, $after: String, $first: Int!) {
-   publication(host: $host) {
-     id
-     posts(after: $after, first: $first) {
-       edges {
-         node {
-           __typename
-           id
-           title
-           slug
-           publishedAt
-           url
-           subtitle
-           brief
-           readTimeInMinutes
-           views
-           author {
-             __typename
-             id
-             username
-             name
-             profilePicture
-           }
-           coverImage {
-             __typename
-             url
-           }
-         }
-         cursor
-       }
-       pageInfo {
-         hasNextPage
-         endCursor
-       }
-     }
-   }
- }`
-;
-
 export const fetchPaginatedBlogPosts = async ({ first, after }: { first?: number | null; after?: string | null }) => {
  try {
-  const blogResponse = await fetch(HASHNODE_API_URL, {
-   method: "POST",
-   headers: {
-     "Content-Type": "application/json",
-   },
-   body: JSON.stringify({
-     query: postsQuery,
-     variables: {
-       host: BLOG_HOSTNAME,
-       first: first ?? INITIAL_POST_COUNT,
-       ...(after && { after })
-     },
-   }),
- });
- const blogData = await blogResponse.json();
- const blogPosts = blogData?.data?.publication?.posts ?? [];
+  const blogData = await request(HASHNODE_API_URL, BlogPostsDocument, {
+    host: BLOG_HOSTNAME,
+    first: first ?? INITIAL_POST_COUNT,
+    ...(after && { after })
+  });
+
+ const blogPosts = blogData?.publication?.posts;
  return blogPosts;
  } catch(err) {
   throw new Error('Error fetching blog posts');
@@ -98,22 +23,13 @@ export const fetchPaginatedBlogPosts = async ({ first, after }: { first?: number
 
 export const getPaginatedPostPagePosts = async ({ first, after }: { first?: number | null, after?: string | null }) => {
  try {
-  const blogResponse = await fetch(HASHNODE_API_URL, {
-   method: "POST",
-   headers: {
-     "Content-Type": "application/json",
-   },
-   body: JSON.stringify({
-     query: postPagePostsQuery,
-     variables: {
-       host: BLOG_HOSTNAME,
-       first: first ?? INITIAL_POST_COUNT,
-       ...(after && { after })
-     },
-   }),
- });
- const blogData = await blogResponse.json();
- const blogPosts = blogData?.data?.publication?.posts ?? [];
+  const blogData = await request(HASHNODE_API_URL, PostPagePostsDocument, {
+    host: BLOG_HOSTNAME,
+    first: first ?? INITIAL_POST_COUNT,
+    ...(after && { after })
+  });
+
+ const blogPosts = blogData?.publication?.posts;
  return blogPosts;
  } catch(err) {
   throw new Error('Error fetching blog posts');
@@ -122,31 +38,27 @@ export const getPaginatedPostPagePosts = async ({ first, after }: { first?: numb
 
 // Hashnode API only gives paginated posts
 export const getAllPostPagePosts = async () => {
- let currentCursor = '';
+ let currentCursor: string | undefined | null = '';
  let hasNext = true;
- let posts: any[] = [];
+ let posts: Post[] = [];
 
  while (hasNext) {
    const blogPostData = await getPaginatedPostPagePosts({ 
     first: 20, 
     ...(currentCursor && { after: currentCursor })
    });
-   const { cursor, hasNextPage, postsArray } = getPostsPaginatedData(blogPostData);
-
-   posts = [...posts, ...postsArray];
-   currentCursor = cursor;
-   hasNext = hasNextPage;
+   if (blogPostData) {
+     const { cursor, hasNextPage, postsArray } = getPostsPaginatedData(blogPostData as any);
+  
+     posts = [...posts, ...postsArray];
+     currentCursor = cursor;
+     hasNext = !!hasNextPage;
+   }
  }
  return posts;
 };
 
-export const getPostsPaginatedData = (blogPosts: {
- edges: any[];
- pageInfo: {
-  hasNextPage: boolean;
-  endCursor: string;
- };
-}) => {
+export const getPostsPaginatedData = (blogPosts: PublicationPostConnection[] & { pageInfo: PageInfo; edges: [PostEdge] }) => {
  return ({
   cursor: blogPosts.pageInfo.endCursor,
   hasNextPage: blogPosts.pageInfo.hasNextPage,

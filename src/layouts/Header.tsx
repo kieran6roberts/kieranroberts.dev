7eff7f0b297c7988055c4a5439cf992adadea501
kieran <kieran6roberts@gmail.com>
2024-12-24 10:41:06 +0000

    refactor: add tech carousel section

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useStickyScroll } from "@hooks/useStickyHeader";
 
 import {
   Tooltip,
@@ -37,7 +38,6 @@ const SocialLink = ({
   githubStars?: number | null | undefined;
 }) => {
   const [isHovered, setIsHovered] = React.useState(false);
-
   const handleMouseEnter = React.useCallback(() => setIsHovered(true), []);
   const handleMouseLeave = React.useCallback(() => setIsHovered(false), []);
 
@@ -83,91 +83,98 @@ interface HeaderProps {
 export const Header = ({ githubStars }: HeaderProps) => {
   const headerRef = React.useRef<HTMLElement>(null);
 
-  return (
-    <header ref={headerRef} className="z-[9999] mx-4 mt-4 bg-l dark:bg-d">
-      <nav
-        role="navigation"
-        className="flex items-center max-w-[1250px] bg-zinc-50 dark:bg-zinc-900 backdrop-opacity-90 backdrop-blur-xl shadow-[0px_148px_41px_0px_rgba(0,0,0,0.00),0px_94px_38px_0px_rgba(0,0,0,0.00),0px_53px_32px_0px_rgba(0,0,0,-0.01),0px_24px_24px_0px_rgba(0,0,0,0.02),0px_6px_13px_0px_rgba(0,0,0,0.02)] border border-zinc-200 dark:border-zinc-800 rounded-full h-[60px] mx-auto justify-between gap-x-4 px-4 md:px-8 lg:px-16 py-3.5"
-      >
-        <Button
-          className="hidden lg:flex m-auto inset-0 h-max absolute pointer-events-none opacity-0 transition-opacity focus:pointer-events-auto focus:opacity-100 link-focus"
-          asLink
-          href="#main"
-        >
-          Skip to content
-        </Button>
-        <section className="flex items-center gap-2 flex-1 min-w-0 max-w-[275px]">
-          <div className="relative flex items-center bg-l dark:bg-d justify-center h-10 w-10 rounded-full">
-            <a href="/" className="link-focus rounded-full">
-              <img
-                src="/Kieran-Avatar.webp"
-                alt="Kieran Roberts header picture"
-                width={40}
-                height={40}
-                loading="eager"
-              />
-            </a>
-          </div>
-          <TooltipProvider delayDuration={0}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <a
-                  className="block w-full truncate text-zinc-600 dark:text-zinc-300 text-sm sm:text-base hover:text-zinc-900 dark:hover:text-zinc-100 cursor-pointer underline link-focus rounded-md"
-                  href={`mailto:${EMAIL_ADDRESS}`}
-                >
-                  {EMAIL_ADDRESS}
-                </a>
-              </TooltipTrigger>
-              <TooltipContent align="center" side="bottom">
-                Email me
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </section>
+  useStickyScroll({ elRef: headerRef });
 
-        <section className="flex items-center gap-x-4 md:gap-x-12">
-          <div className="flex items-center gap-x-2 md:gap-x-6 text-sm">
-            <SocialLink
-              href={LINKEDIN_PROFILE_URL}
-              label="LinkedIn"
-              Icon={<LinkedInSVG />}
-            />
-            <span className="hidden md:block text-zinc-400 dark:text-zinc-500">
-              |
-            </span>
-            <SocialLink
-              href={GITHUB_PROFILE_URL}
-              label="GitHub"
-              Icon={<GithubSVG />}
-              githubStars={githubStars}
-            />
-            <span className="hidden md:block text-zinc-400 dark:text-zinc-500">
-              |
-            </span>
-            <SocialLink href={X_PROFILE_URL} label="X" Icon={<XSVG />} />
-            <span className="hidden md:block text-zinc-400 dark:text-zinc-500">
-              |
-            </span>
-            <SocialLink
-              href={BLUESKY_PROFILE_URL}
-              label="Bluesky"
-              Icon={<BlueSkySVG />}
-            />
-          </div>
-          <ThemeToggle />
-        </section>
-      </nav>
-      <div className="max-w-[1250px] flex justify-end px-4 md:px-8 lg:px-16 mx-auto text-center bg-l dark:bg-d text-zinc-500 dark:text-zinc-400 py-4 text-sm lg:text-base">
-        Software developer{" "}
+  return (
+    <>
+      <header
+        ref={headerRef}
+        className="sticky top-4 z-[9999] mx-4 bg-l dark:bg-d"
+      >
+        <nav
+          role="navigation"
+          className="flex items-center max-w-[1250px] bg-zinc-50 dark:bg-zinc-900 backdrop-opacity-90 backdrop-blur-xl shadow-[0px_148px_41px_0px_rgba(0,0,0,0.00),0px_94px_38px_0px_rgba(0,0,0,0.00),0px_53px_32px_0px_rgba(0,0,0,-0.01),0px_24px_24px_0px_rgba(0,0,0,0.02),0px_6px_13px_0px_rgba(0,0,0,0.02)] border border-zinc-200 dark:border-zinc-800 rounded-full h-[60px] mx-auto justify-between gap-x-4 px-4 md:px-8 lg:px-16 py-3.5"
+        >
+          <Button
+            className="hidden lg:flex m-auto inset-0 h-max absolute pointer-events-none opacity-0 transition-opacity focus:pointer-events-auto focus:opacity-100 link-focus"
+            asLink
+            href="#main"
+          >
+            Skip to content
+          </Button>
+          <section className="flex items-center gap-2 flex-1 min-w-0 max-w-[275px]">
+            <div className="relative flex items-center bg-l dark:bg-d justify-center h-10 w-10 rounded-full">
+              <a href="/" className="link-focus rounded-full">
+                <img
+                  src="/Kieran-Avatar.webp"
+                  alt="Kieran Roberts header picture"
+                  width={40}
+                  height={40}
+                  loading="eager"
+                />
+              </a>
+            </div>
+            <TooltipProvider delayDuration={0}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <a
+                    className="block w-full truncate text-zinc-600 dark:text-zinc-300 text-sm sm:text-base hover:text-zinc-900 dark:hover:text-zinc-100 cursor-pointer underline link-focus rounded-md"
+                    href={`mailto:${EMAIL_ADDRESS}`}
+                  >
+                    {EMAIL_ADDRESS}
+                  </a>
+                </TooltipTrigger>
+                <TooltipContent align="center" side="bottom">
+                  Email me
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </section>
+
+          <section className="flex items-center gap-x-4 md:gap-x-12">
+            <div className="flex items-center gap-x-2 md:gap-x-6 text-sm">
+              <SocialLink
+                href={LINKEDIN_PROFILE_URL}
+                label="LinkedIn"
+                Icon={<LinkedInSVG />}
+              />
+              <span className="hidden md:block text-zinc-400 dark:text-zinc-500">
+                |
+              </span>
+              <SocialLink
+                href={GITHUB_PROFILE_URL}
+                label="GitHub"
+                Icon={<GithubSVG />}
+                githubStars={githubStars}
+              />
+              <span className="hidden md:block text-zinc-400 dark:text-zinc-500">
+                |
+              </span>
+              <SocialLink href={X_PROFILE_URL} label="X" Icon={<XSVG />} />
+              <span className="hidden md:block text-zinc-400 dark:text-zinc-500">
+                |
+              </span>
+              <SocialLink
+                href={BLUESKY_PROFILE_URL}
+                label="Bluesky"
+                Icon={<BlueSkySVG />}
+              />
+            </div>
+            <ThemeToggle />
+          </section>
+        </nav>
+      </header>
+      <div className="max-w-[1250px] w-full flex gap-1 justify-end mx-auto px-4 md:px-8 lg:px-16 text-center bg-l dark:bg-d text-zinc-500 dark:text-zinc-400 py-6 text-sm lg:text-base">
+        <span className="">Software developer</span>
         <a
           href="https://hashnode.com"
           target="_blank"
           rel="noreferrer"
-          className="block ml-1 text-zinc-500 dark:text-zinc-400 hover:text-zinc-800 dark:hover:text-zinc-200 cursor-pointer rounded-md font-semibold outline-none link-focus"
+          className="block text-zinc-500 dark:text-zinc-400 hover:text-zinc-800 dark:hover:text-zinc-200 cursor-pointer rounded-md font-semibold outline-none link-focus"
         >
           @Hashnode
         </a>
       </div>
-    </header>
+    </>
   );
 };

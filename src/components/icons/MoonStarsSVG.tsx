de84b05fa2f761908e9415f67a61cd8862b36e13
kieran <kieran6roberts@gmail.com>
2025-01-08 20:46:14 +0000

    refactor: mode react -> astro updates + formatting

--- src/components/icons/MoonStarsSVG.tsx
+++ src/components/icons/MoonStarsSVG.tsx
@@ -1,5 +1,3 @@
-import * as React from 'react';
-
 export const MoonStarsSVG = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
     <rect width="256" height="256" fill="none" />
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,20 +1,20 @@
 import {
-  BlogPostsDocument,
-  PostPagePostsDocument,
-  type BlogPostsQuery,
-  type PostPagePostsQuery,
+	BlogPostsDocument,
+	PostPagePostsDocument,
+	type BlogPostsQuery,
+	type PostPagePostsQuery,
 } from 'generated/graphql';
 
 export type BlogPosts = NonNullable<BlogPostsQuery['publication']>['posts'];
 export type PostPagePosts = NonNullable<PostPagePostsQuery['publication']>['posts'];
 
 export type Experience = {
-  title: string;
-  company: string;
-  location: string;
-  date: string;
-  type: string;
-  logo: React.ReactNode;
-  responsibilities: string[];
-  image: ImageMetadata | null;
+	title: string;
+	company: string;
+	location: string;
+	date: string;
+	type: string;
+	logo: React.ReactNode;
+	responsibilities: string[];
+	image: ImageMetadata | null;
 };

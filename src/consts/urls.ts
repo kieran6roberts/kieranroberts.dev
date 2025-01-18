56e5f1d172897abe776a4962bbb45e978548c4f5
kieran <kieran6roberts@gmail.com>
2025-01-18 10:16:11 +0000

    refactor: update linkedin url

--- src/consts/urls.ts
+++ src/consts/urls.ts
@@ -3,4 +3,4 @@ export const DOMAIN_NAME = 'kieranroberts.dev';
 
 export const X_PROFILE_URL = 'https://x.com/Kieran6dev';
 export const GITHUB_PROFILE_URL = 'https://github.com/kieran6roberts';
-export const LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/kieran-roberts-00517b178/';
+export const LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/kieran6roberts/';

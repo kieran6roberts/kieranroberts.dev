d78ef38361db4c038352ba8b355d5c7eeedd26c6
kieran <kieran6roberts@gmail.com>
2025-02-08 16:30:08 +0000

    chore: various tailwind fixes for v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,4 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,ts,jsx,tsx}'],
 	plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Lato', 'sans-serif'],
-				heading: ['Barlow', 'sans-serif'],
-			},
-			colors: {
-				'accent-darkest': '#6F61C0',
-				'accent-dark': '#A084E8',
-				'accent-bright': '#8BE8E5',
-				'accent-brightest': '#D5FFE4',
-			},
-		},
-	},
-	darkMode: ['class'],
 };

c75ad208e81a267057b5738dc430ec0bc55497d4
kieran <kieran6roberts@gmail.com>
2025-02-08 15:42:16 +0000

    feat: upgrade to tailwindv4 + prose style blog content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 export default {
 	content: ['./src/**/*.{astro,html,js,ts,jsx,tsx}'],
-	plugins: [require('tailwindcss-animate')],
+	plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 	theme: {
 		extend: {
 			fontFamily: {
